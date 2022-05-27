ad112b55d4dd70a43b4954d5cb00796740a6913a
chanduvaluri <chanduvaluri3@gmail.com>
2022-05-27 07:26:46 +0000

    GET POST PUT DELETE

--- Day 4 Spring boot/SpringApplication/src/main/java/springapplication/service/CustomerService.java
+++ Day 4 Spring boot/SpringApplication/src/main/java/springapplication/service/CustomerService.java
@@ -1,8 +1,18 @@
 package springapplication.service;
 
+import java.util.List;
 import springapplication.bean.Customer;
 
 public interface CustomerService {
 
-	Customer addCustomer(Customer customer);
+	Customer addCustomer(Customer customer); //POST
+	
+	List<Customer> getAllListofCustomers(); //GET
+	
+	Customer updateCustomer(Customer customer, long id); //PUT
+	
+	
+	void deleteById(long id); //DELETE
+	
+	
 }
--- Day 4 Spring boot/SpringApplication/src/main/java/springapplication/serviceImp/CustomerServiceimp.java
+++ Day 4 Spring boot/SpringApplication/src/main/java/springapplication/serviceImp/CustomerServiceimp.java
@@ -1,21 +1,49 @@
 package springapplication.serviceImp;
 
+import java.util.List;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 
 import springapplication.bean.Customer;
 import springapplication.repository.MyLocalRepository;
 import springapplication.service.CustomerService;
-
+@Service
 public class CustomerServiceimp implements CustomerService {
 	
 	@Autowired
 	MyLocalRepository mylocalrepository;
 	
-	@Override
+	@Override // POST method
 	public Customer addCustomer(Customer customer) {
 		Customer customeradded = mylocalrepository.save(customer);
 		
 		return customeradded;
 	}
 
+	@Override //GET method
+	public List<Customer> getAllListofCustomers() {
+		List<Customer> listofcustomers = mylocalrepository.findAll();
+		return listofcustomers;
+	}
+
+	@Override //PUT method
+	public Customer updateCustomer(Customer customer, long id) {
+		
+		customer.setId(id);
+		Customer updatedcustomer = mylocalrepository.save(customer);
+
+		return updatedcustomer;
+	}
+
+	@Override
+	public void deleteById(long id) {
+		mylocalrepository.deleteById(id);// delete method 
+		
+	}
+
+	
+	
 }
